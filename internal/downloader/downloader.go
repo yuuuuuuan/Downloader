03ee04910fc6c651d973397646265a0c0323f3c9
Hangyuan Zou <99110203+yuuuuuuan@users.noreply.github.com>
2025-03-18 06:38:24 +0000

    add

--- internal/downloader/downloader.go
+++ internal/downloader/downloader.go
@@ -1,1 +1,22 @@
 package downloader
+
+type Downloader struct{
+    URL      string
+    FilePath string
+}
+
+func (d *Downloader) StartDownload() error {
+    fmt.Println("Starting download:", d.URL)
+    // 使用网络模块下载文件
+    fileData, err := network.DownloadFile(d.URL)
+    if err != nil {
+        return err
+    }
+    // 使用存储模块保存文件
+    err = storage.SaveFile(d.FilePath, fileData)
+    if err != nil {
+        return err
+    }
+    fmt.Println("Download complete!")
+    return nil
+}
--- storage/files.go
+++ storage/files.go
@@ -1,1 +1,15 @@
 package storage
+
+func SaveFile(filePath string, data []byte) error {
+    file, err := os.Create(filePath)
+    if err != nil {
+        return fmt.Errorf("failed to create file: %v", err)
+    }
+    defer file.Close()
+
+    _, err = file.Write(data)
+    if err != nil {
+        return fmt.Errorf("failed to write file: %v", err)
+    }
+    return nil
+}
